bfe93dbc807ef820d36e279d4b85fd10d46acefa
Michael Wahl <michaelwahl9@gmail.com>
2019-09-30 16:21:18 -0500

    fourth challenge

--- 130-LoopsArraysAndConcatenation.js
+++ 130-LoopsArraysAndConcatenation.js
@@ -1,1 +1,12 @@
 console.log('130-LoopsArraysAndConcatenation.js working')
+
+function arrayConcatenator(array, index){
+    let output = '';
+    for(let i = index; i < array.length; i++){
+        let temp = array[i] + ' '
+        output += temp;
+    }
+    return output;
+}
+
+console.log(arrayConcatenator(['prime', 'nalgene', 'apple', 'pen', 'shirt', 'envelope'], 2));
