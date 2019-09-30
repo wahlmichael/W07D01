9f427a66c857fd047341f4070216b77fdff481d0
Michael Wahl <michaelwahl9@gmail.com>
2019-09-30 16:44:30 -0500

    fifth challenge

--- 140-LoopsAndTwoDimensionalArrays.js
+++ 140-LoopsAndTwoDimensionalArrays.js
@@ -1,1 +1,15 @@
-console.log('140-LoopsAndTwoDimensionalArrays.js working')
+console.log('140-LoopsAndTwoDimensionalArrays.js working');
+
+let sum = 0;
+
+function arraySumFinder(array){
+    for(let i = 0; i < array.length; i++){
+        for(let n = 0; n < array[i].length; n++){
+        let value = array[i][n];
+        sum += value;
+        }
+    }
+    return sum;
+}
+
+console.log(arraySumFinder([[1,2,3], [4,5], [6,7,8]]));
