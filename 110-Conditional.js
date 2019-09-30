8cec493e8d56c7420e5437d4dd2f45a72e099a74
Michael Wahl <michaelwahl9@gmail.com>
2019-09-30 16:07:03 -0500

    second challenge

--- 110-Conditional.js
+++ 110-Conditional.js
@@ -1,1 +1,15 @@
 console.log('110-Conditional.js working')
+
+function sizeChecker(num1, num2){
+    if(num1 > num2){
+        return 'The first number was bigger!'
+    }else if(num2 > num1){
+        return 'The second numer was bigger!'
+    }else{
+        return 'The numbers are the same!'
+    }
+}
+
+console.log(sizeChecker(6, 2));
+console.log(sizeChecker(-2, 6));
+console.log(sizeChecker(6, 6));
