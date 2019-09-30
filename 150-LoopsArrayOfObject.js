6de34724153306477c44693edc16041ac060bc7e
Michael Wahl <michaelwahl9@gmail.com>
2019-09-30 16:50:25 -0500

    sixth challenge

--- 150-LoopsArrayOfObject.js
+++ 150-LoopsArrayOfObject.js
@@ -1,1 +1,17 @@
-console.log('150-LoopsArrayOfObject working')
+console.log('150-LoopsArrayOfObject working');
+
+let total = 0;
+
+function experienceAdder(array){
+    for(let i = 0; i < array.length; i++){
+        value = array[i].yearsOfExperience;
+        total += value;
+    }
+    return total;
+}
+
+console.log(experienceAdder([
+    { name: "Tony", yearsOfExperience: 10 }, 
+    { name: "Carla", yearsOfExperience: 4 },
+    { name: "Kris", yearsOfExperience: 14 }
+]));
